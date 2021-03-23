f67a262ca7a9d29459790a4cddd67e1f536aa144
Daniela8426 <61909378+Daniela8426@users.noreply.github.com>
2021-03-22 21:31:29 -0500

    Delete utopia directory

--- utopia/php/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conexion= new mysqli("localhost","root","","comprautopia");
-    if($conexion->connect_error)
-    {
-        die("fallo la conexion" . mysqli_connect_errno());
-    }
-?>
